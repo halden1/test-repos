7fd9b08155a5683f28897c5933f54f0b33f24ec7
halden1 <halden.junk@gmail.com>
2016-06-23 13:08:32 +1000

    Tidy up code

--- Assets/Scripts/ProceduralGeneration.cs
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -2,19 +2,19 @@
 using System.Collections;
 
 public class ProceduralGeneration : MonoBehaviour {
-    public int xSize = 200;
-    public int ySize = 200;
+    public int xSize = 100;
+    public int ySize = 100;
     public GameObject cube;
     // Use this for initialization
     void Start () {
-	    for(int x = 0;x< xSize; x++)
+	    for(int x = 0;x < xSize; x += 1 )
         {
-            for (int y = 0; y < ySize; y++)
+            for (int y = 0; y < ySize; y += 1 )
             {
-               GameObject tmp =Instantiate(cube, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+               GameObject tmp = Instantiate(cube, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
                if (x % 2 == 0) { tmp.GetComponent<Renderer>().material.SetColor("_Color", new Color(255, 0, 0, 1));}
                tmp.AddComponent<UnitBehavior>();
-                tmp.GetComponent<UnitBehavior>().posSetter(x, y);
+               tmp.GetComponent<UnitBehavior>().posSetter(x, y);
             }
         }
 	}
