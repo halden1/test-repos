f06e4e401452fa112165d8be95381ff56b9eabea
halden1 <halden.junk@gmail.com>
2016-06-23 13:39:36 +1000

    fixing problems

--- Assets/Scripts/ProceduralGeneration.cs
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -15,9 +15,9 @@ public class ProceduralGeneration : MonoBehaviour {
                if (x % 2 == 0) { tmp.GetComponent<Renderer>().material.SetColor("_Color", new Color(255, 0, 0, 1));}
                tmp.AddComponent<UnitBehavior>();
                tmp.GetComponent<UnitBehavior>().posSetter(x, y);
-            }
-        }
-	}
+            }//
+        }//
+	}//
 	
 	// Update is called once per frame
 	void Update () {
