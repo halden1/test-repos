60272d578dc31c8a91db76b2b44dd62b4157be19
halden1 <halden.junk@gmail.com>
2016-06-23 12:19:17 +1000

    breaking the loop script

--- Assets/Scripts/ProceduralGeneration.cs
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -7,9 +7,9 @@ public class ProceduralGeneration : MonoBehaviour {
     public GameObject cube;
     // Use this for initialization
     void Start () {
-	    for(int x = 0;x< xSize; x++)
+	    for(int x = 0;x> xSize; x++)
         {
-            for (int y = 0; y < ySize; y++)
+            for (int y = 0; y > ySize; y++)
             {
                GameObject tmp =Instantiate(cube, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
                if (x % 2 == 0) { tmp.GetComponent<Renderer>().material.SetColor("_Color", new Color(255, 0, 0, 1));}
